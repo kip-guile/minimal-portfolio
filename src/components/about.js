5f0908f134fb11e1a10934acb9ac87179c51faf0
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-08-03 11:18:18 +0100

    animation for contacts

--- src/components/about.js
+++ src/components/about.js
@@ -1,73 +1,15 @@
 import React from "react"
 import { Box, Text, Flex, PseudoBox } from "@chakra-ui/core"
 import { contact } from "./data"
+import { motion } from "framer-motion"
 import ReactTypingEffect from "react-typing-effect"
-// import {
-//   DiJavascript1,
-//   DiReact,
-//   DiCss3,
-//   DiGit,
-//   DiPostgresql,
-//   DiMongodb,
-//   DiTravis,
-//   DiMysql,
-//   DiLinux,
-// } from "react-icons/di";
+import { DiGit } from "react-icons/di"
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai"
-// import {
-//   FaGoodreads,
-//   FaNodeJs,
-//   FaVuejs,
-//   FaLess,
-//   FaPython,
-//   FaTrello,
-//   FaAws,
-// } from "react-icons/fa";
-// import { BsGraphUp } from "react-icons/bs";
-// import { GiJesterHat } from "react-icons/gi";
+import { FaGoodreads } from "react-icons/fa"
 import { bio } from "./data"
 
-// const skills = [
-//   { tag: DiJavascript1, name: "JavaScript" },
-//   { tag: DiReact, name: "React" },
-//   { tag: FaNodeJs, name: "NodeJS" },
-//   { tag: FaVuejs, name: "Vue" },
-//   { tag: AiFillHtml5, name: "HTML5" },
-//   { tag: FaPython, name: "Python" },
-//   { tag: DiMysql, name: "MySQL" },
-//   { tag: DiCss3, name: "CSS3" },
-//   { tag: DiReact, name: "Redux" },
-//   { tag: DiPostgresql, name: "PostGresDB" },
-//   { tag: DiMongodb, name: "MongoDB" },
-//   { tag: FaLess, name: "Less" },
-// ];
-
-// const baSkills = [
-//   { tag: DiGit, name: "Git" },
-//   { tag: FaAws, name: "AWS" },
-//   { tag: FaTrello, name: "Trello" },
-//   { tag: GiJesterHat, name: "Jest" },
-//   { tag: DiTravis, name: "TravisCI" },
-//   { tag: DiLinux, name: "Linux" },
-//   // { tag: BsGraphUp, name: "Requirement Management" },
-//   { tag: BsGraphUp, name: "Elicitation" },
-//   { tag: BsGraphUp, name: "Quality Assurance" },
-// ];
-
-// const tag = (name, Logo, color, key) => (
-//   <Box mb={5} key={key}>
-//     <Tag mr={2} mb={2} variantColor={color} size="md">
-//       <Logo
-//         style={{
-//           backgroundColor: "hsla(0,0%,100%,0.88)",
-//           color: "#282c35",
-//           marginRight: "5px",
-//         }}
-//       />
-//       <TagLabel>{<p style={{ fontSize: "0.8em" }}>{name}</p>}</TagLabel>
-//     </Tag>
-//   </Box>
-// );
+const MotionFlex = motion.custom(Flex)
+const MotionPseudoBox = motion.custom(PseudoBox)
 
 const paragraphs = (text, i) => (
   <Text
@@ -119,29 +61,6 @@ export const middleBox = (darkMode) => (
     p={{ md: 0, xs: 5 }}
     minHeight={{ md: "35rem" }}
   >
-    {/* <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent={{ md: "space-around", xs: "center" }}
-      flexBasis={{ md: "15%" }}
-      h={{ md: "90%" }}
-    >
-      <Text
-        fontSize="lg"
-        fontWeight="bold"
-        // mt={5}
-        mb={5}
-        fontFamily="Cousine, monospace"
-        color={darkMode ? "#ffa7c4" : "dodgerblue"}
-        textAlign="center"
-      >
-        Tools
-      </Text>
-      <Flex wrap="wrap" justifyContent="space-between">
-        {skills.map((skill, i) => tag(skill.name, skill.tag, "blue", i))}
-        {baSkills.map((skill, i) => tag(skill.name, skill.tag, "red", i))}
-      </Flex>
-    </Box> */}
     <Box
       display="flex"
       flexDirection="column"
@@ -184,19 +103,29 @@ export const contactBox = (darkMode) => (
       justifyContent={{ md: "space-between" }}
       textAlign={{ md: "justify", xs: "center" }}
     >
-      <Flex
+      <MotionFlex
         mt={{ md: 4, xs: 8 }}
         mb={{ xs: 4 }}
+        animate={{ y: [50, -50, 0] }}
+        transition={{
+          ease: "easeOut",
+          duration: 1.5,
+          times: [0, 0.2, 1],
+        }}
         width={{ md: "100%" }}
         justifyContent={{ md: "center", xs: "center" }}
         alignItems="center"
         color={darkMode ? "#282c35" : "hsla(0,0%,100%,0.88)"}
       >
         {contact.map((contact) => (
-          <PseudoBox
+          <MotionPseudoBox
             target="_blank"
             href={contact.url}
             as="a"
+            whileHover={{
+              scale: 1.2,
+              transition: { duration: 0.8 },
+            }}
             _hover={{
               backgroundColor: darkMode ? "#ffa7c4" : "dodgerblue",
               cursor: "pointer",
@@ -208,9 +137,9 @@ export const contactBox = (darkMode) => (
             bg={darkMode ? "#ffecb2" : "#282c35"}
           >
             <AiFillLinkedin fontSize="2rem" />
-          </PseudoBox>
+          </MotionPseudoBox>
         ))}
-      </Flex>
+      </MotionFlex>
     </Box>
   </Box>
 )
