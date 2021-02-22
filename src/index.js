096f1584685f4514c3c54c7b4ff1239b95dcbf9e
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2021-02-22 02:38:58 +0100

    fix: fix devt

--- src/index.js
+++ src/index.js
@@ -15,13 +15,7 @@ const middleware = [thunk]
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
-  )
+  compose(applyMiddleware(...middleware))
 )
 
 ReactDOM.render(
